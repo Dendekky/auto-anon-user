4f0fcd5d0c3723761776d505303a259c0d0470b9
Ibrahim <adeniyiib360@gmail.com>
2020-11-16 13:24:38 +0100

    chore: set up express app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # auto-anon-user
+
+### Quick Start
+* Install dependencies by running `npm install`.
+* Run `npm run dev` to start the local development server.
+
+<br />
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';/* eslint-disable linebreak-style */
+// import mongoose from 'mongoose';
+
+// mongoose.connect(, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+// })
+  // .catch(err => console.log(err));
+
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.use(cors());
+
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.json());
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    errors: {
+      message: err.message
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`App Listening on port ${PORT}`));
+
+export default app;
